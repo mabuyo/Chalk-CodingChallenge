297b441153fabd37a530e8c03bc64f119c6b9b66
mabuyo <mabuyo@ualberta.ca>
2017-10-19 19:59:16 -0400

    Can retrieve list of teachers from API endpoint and display full name in generic list form.

--- src/components/TeacherList.js
+++ src/components/TeacherList.js
@@ -1,18 +1,32 @@
 import React, { Component } from "react";
 
 class TeacherList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { teachers: [] };
+  }
+
+  componentWillMount() {
+    this.getTeachers();
+  }
+
+  getTeachers() {
+    const url = "https://cdn.chalk.com/misc/sample_teachers.json";
+    fetch(url)
+      .then(result => result.json())
+      .then(result => this.setState({ teachers: result }));
+  }
+
+  renderTeachersListItems() {
+    return this.state.teachers.map(teacher => (
+      <li key={teacher.id}>{teacher.first_name} {teacher.last_name}</li>
+    ));
+  }
+
   render() {
     return (
       <div style={styles.listContainer}>
-        <ul>
-          <li>Teacher</li>
-          <li>Teacher</li>
-          <li>Teacher</li>
-          <li>Teacher</li>
-          <li>Teacher</li>
-          <li>Teacher</li>
-          <li>Teacher</li>
-        </ul>
+        <ul>{this.renderTeachersListItems()}</ul>
       </div>
     );
   }
@@ -20,7 +34,7 @@ class TeacherList extends Component {
 
 const styles = {
   listContainer: {
-    backgroundColor: "blue",
+    backgroundColor: "#78B3F3",
     width: "80%",
     marginTop: "20px",
     borderRadius: "5px"
